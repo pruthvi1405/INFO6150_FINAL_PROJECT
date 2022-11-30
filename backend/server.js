209e0ce0ede405968f44291e9a435e8fca635794
shubhamsable2000 <sable.s@northeastern.edu>
2022-11-29 19:54:17 -0500

    Email Sending

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,5 @@ app.use(errorHandler);
 //server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running ${PORT}`));
+
+//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
