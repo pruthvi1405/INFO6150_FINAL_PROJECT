3645ff35e550c6d848e694133b280f69f7fa2861
shubhamsable2000 <sable.s@northeastern.edu>
2022-12-05 16:40:14 -0500

    Post Category

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const userRoutes = require("./route/users/usersRoute");
 const { errorHandler, notFound } = require("./middlewares/error/errorHandler");
 const postRoute = require("./route/posts/postRoute");
 const commentRoutes = require("./route/comments/commentRoute");
+const emailMsgRoute = require("./route/emailMsg/emailMsgRoute");
+const categoryRoute = require("./route/category/categoryRoute");
 
 const app = express();
 //DB
@@ -20,6 +22,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoute);
 //comment routes
 app.use("/api/comments", commentRoutes);
+//email msg
+app.use("/api/email", emailMsgRoute);
+//category route
+app.use("/api/category", categoryRoute);
 //err handler
 app.use(notFound);
 app.use(errorHandler);
